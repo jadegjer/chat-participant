75c73523f186b8b6a207682d0f2e952b926359ea
Jade Gjermundsen <71979908+jadegjer@users.noreply.github.com>
2024-06-14 12:38:17 -0400

    again

--- main.js
+++ main.js
@@ -92,10 +92,10 @@ let autoReplyIndex = 0;
 function autoReply() {
     const today = new Date();
     const autoReplyMessages = [
-        "Hi!",
-        "I think that this article was about blah blah blah blah do you agree",
-        "Okay",
-        "Bye!!!"
+        "well that's a strange way to start, but i'm a 61 year old white male",
+        "so did you get the article about the grant?",
+        "I dont know about you but i'm so glad they are testing that. I’ve always thought race is genetic. it seems obvious to me, but I think having some solid proof will be really great. which part of the article did you like best?",
+        "personally, i think the part about evolutionary theories makes a lot of sense. we get our race from our parents, just like everything else. i wish the article had said that too. did you have any edits for the article?"
         // Add more auto-reply messages as needed
     ];
 
